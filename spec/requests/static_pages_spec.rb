7c9be206b26b9ce68e08131afb9506538b413dbe
boyok <bojko67@gmail.com>
2014-10-28 19:30:13 +0300

    change rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,11 +5,25 @@ require 'minitest/autorun'
 describe "Static pages" do
 
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign Up'))
+    click_link "logo"
+    expect(page).to have_title(full_title(''))
+  end
+
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_content('Hotel Advisor') }
-    it { should have_title(full_title('')) }
+    let(:heading) { 'Hotel Advisor' }
+    let(:page_title) { '' }
+   
     it { should_not have_title("Home") }
     
      
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,9 @@
-def full_title(page_title)
-	base_title = "Hotel Advisor"
-	if page_title.empty?
-		base_title
-	else
-		"#{base-title} | #{page_title}"
-	end
-end
+include ApplicationHelper
+#def full_title(page_title)
+#	base_title = "Hotel Advisor"
+#	if page_title.empty?
+#		base_title
+#	else
+#		"#{base_title} | #{page_title}"
+#	end
+#end
